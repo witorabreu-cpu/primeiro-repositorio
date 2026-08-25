e135e4b7fadf62574f7c1faf3a43ed01fa63e5b3
witorabreu-cpu <witor.abreu@escola.pr.gov.br>
2026-08-24 22:12:35 -0300

    aula24/08

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const botoes = document.querySelectorALL("button");
+
 botoes..forEach(function (botao) {
     let curtiu= false;
     botao.addEventListener("click" , botaoClicado);
     function botaoClicado(){
+        console.console.log("fui clicado");
+        let texto = botao.querySelector("span")
         if (curtiu === false) {
             texto.textContent++;
             curtiu= true;
